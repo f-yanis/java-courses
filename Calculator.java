20494121bf783d7e4d113a891e0c1067dc96a7e3
f-yanis <f.yanis48@gmail.com>
2017-06-08 11:49:47 +0300

    lesson 3_2

--- Calculator.java
+++ Calculator.java
@@ -16,6 +16,46 @@
 		 }
 	 }
 	 
+	 /*
+	 * Вычитаем аргументы.
+	 * @param params Аргументы разницы
+	 */
+	 public void sub(int ... params){
+		 for (Integer param : params){
+			this.result -= param;
+		 }
+	 }
+	 
+	 /*
+	 * Умножаем аргументы.
+	 * @param params Аргументы умножения
+	 */
+	 public void mult(int ... params){
+		 for (Integer param : params){
+			this.result *= param;
+		 }
+	 }
+	 
+	 /*
+	 * Делим аргументы.
+	 * @param params Аргументы деления
+	 */
+	 public void div(int ... params){
+		 for (Integer param : params){
+			this.result /= param;
+		 }
+	 }
+	 
+	  /*
+	 * Находим степень аргумента.
+	 * @param params Аргументы суммирования
+	 */
+	 public void exp(int ... params){
+		 for (Integer param : params){
+			this.result ^= param;
+		 }
+	 }
+	 
 	 /*Получить результат.
 	 * @return результат вычисления.
 	 */
--- InteractRunner.java
+++ InteractRunner.java
@@ -12,12 +12,37 @@ public class InteractRunner{
 			while(!exit.equals("да")){
 				System.out.print("Введите первое число: ");
 				String fitst = reader.next();
+				
 				System.out.print("Введите второе число: ");
 				String second = reader.next();
-				calc.add(Integer.valueOf(fitst), 
-						 Integer.valueOf(second));
-				System.out.println("Резултат: " + calc.getResult());
-				calc.cleanResult();
+				
+				System.out.print("Введите знак операции: ");
+				String ariph = reader.next();
+				
+				if (ariph.equals("+")){
+					calc.add(Integer.valueOf(fitst), 
+							 Integer.valueOf(second));
+					System.out.println("Резултат: " + calc.getResult());
+				} else if (ariph.equals("-")){
+					calc.sub(Integer.valueOf(fitst), 
+							 Integer.valueOf(second));
+					System.out.println("Резултат: " + calc.getResult());
+				} else if (ariph.equals("*")){
+					calc.mult(Integer.valueOf(fitst), 
+							  Integer.valueOf(second));
+					System.out.println("Резултат: " + calc.getResult());
+				} else if (ariph.equals("/")){
+					calc.div(Integer.valueOf(fitst), 
+							 Integer.valueOf(second));
+					System.out.println("Резултат: " + calc.getResult());
+				} else if (ariph.equals("^")){
+					calc.exp(Integer.valueOf(fitst), 
+							 Integer.valueOf(second));
+					System.out.println("Резултат: " + calc.getResult());
+				} else if (ariph.equals("00")){
+					calc.cleanResult();
+				}
+			
 				System.out.println("Выйти?: да/нет");
 				exit = reader.next();
 			}
